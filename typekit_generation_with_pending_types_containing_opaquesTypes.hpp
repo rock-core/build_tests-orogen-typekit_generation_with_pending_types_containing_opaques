1a228b09c572c7aafd3de94baa30dc4c7166e6de
Sylvain Joyeux <sylvain.joyeux@13robotics.com>
2019-04-28 14:02:14 -0300

    initial commit by rock-create-orogen

---
+++ typekit_generation_with_pending_types_containing_opaquesTypes.hpp
@@ -0,0 +1,14 @@
+#ifndef typekit_generation_with_pending_types_containing_opaques_TYPES_HPP
+#define typekit_generation_with_pending_types_containing_opaques_TYPES_HPP
+
+#include <base/Pose.hpp>
+
+namespace typekit_generation_with_pending_types_containing_opaques {
+    struct TypeWithOpaque
+    {
+        base::Position position;
+    };
+}
+
+#endif
+
